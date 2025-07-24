536b9d7e1ac08b63665da601a4514d93fc80d98f
kwakuvovota <vovotadevs@gmail.com>
2025-07-24 02:19:14 -0400

    Remove title on posts in Carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -50,10 +50,6 @@ export const PortraitCarousel = () => {
           </div>
         ))}
       </div>
-
-      <div className="text-center mt-4 text-gray-800 dark:text-gray-200 font-semibold text-lg">
-        {images[current]?.titre}
-      </div>
     </div>
   );
 };
